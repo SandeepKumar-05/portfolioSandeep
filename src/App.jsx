5eb50b2d45ad14011488eaad58cfce5f4fd97de4
SandeepKumar-05 <sandeepkumarps005@gmail.com>
2025-06-23 16:10:25 +0530

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Navbar from './components/Navbar'
 export default function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navbar/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/Navbar.css'
+function Navbar() {
+  return (
+    <header>
+        <div className="logo">
+            <h1>Sandeep</h1>
+        </div>
+        <div className="connect">
+            <h5>Connect</h5>
+        </div>
+    </header>
+  )
+}
+
+export default Navbar
