e7c44c46fa68667e91bdeb9b465845614a6737a3
SandeepKumar-05 <sandeepkumarps005@gmail.com>
2025-06-24 21:10:12 +0530

    Hero Updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,35 +4,35 @@ import '../styles/Hero.css'; // We'll create this next
 import profilePic from '../assets/profilePic.jpg'
 const Hero = () => {
   return (
-    <section className="hero-section">
-      <div className="hero-content">
-        <div className="typewriter-container">
-            <h1>Hi ,I'm a</h1>
-          <Typewriter
-            options={{
-              strings: [
-                'frontend developer',
-                'ai-ml developer'
-              ],
-              autoStart: true,
-              loop: true,
-              delay: 70,
-              deleteSpeed: 30,
-              cursorClassName: 'typewriter-cursor'
-            }}
-          />
-          
+    <section className="heroSection">
+      <div className="heroContent">
+            <div className="typewriterContainer">
+                <h1>Hi ,I'm a</h1>
+            <Typewriter
+                options={{
+                strings: [
+                    'frontend developer',
+                    'ai-ml developer'
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 70,
+                deleteSpeed: 30,
+                cursorClassName: 'typewriter-cursor'
+                }}
+            />
+            
+            </div>
         </div>
         <div className="heroSubHeading">
-            <p>I'm a Frontend Developer and AI/ML enthusiast who loves building intuitive user interfaces and smart, data-driven applications using React, Python, and deep learning. </p>
+                <p>I'm a Frontend Developer and AI/ML enthusiast who loves building intuitive user interfaces and smart, data-driven applications using React, Python, and deep learning. </p>
         </div>
-        <button className="cta-button">
-         Contact
+        <button className="cntButton">
+            Contact
         </button>
-      </div>
-      <div className="heroImg">
-        <img src={profilePic} alt="img" />
-      </div>
+        <div className="heroImg">
+            <img src={profilePic} alt="img" />
+        </div>
     </section>
   );
 };
