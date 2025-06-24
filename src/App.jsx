21d03ba29b67a5fe2df2c8a8e7f955a698eeb5e3
SandeepKumar-05 <sandeepkumarps005@gmail.com>
2025-06-24 16:05:20 +0530

    Hero Section Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import Navbar from './components/Navbar';
 import './index.css'
+import Home from './HomePage/Home';
 export default function App() {
   return (
     <div>
       <BrowserRouter>
       <Navbar/>
         <Routes>
-          {/* <Route index element={<Navbar/>}/> */}
+          <Route index element={<Home/>}/>
         </Routes>
       </BrowserRouter>
     </div>
