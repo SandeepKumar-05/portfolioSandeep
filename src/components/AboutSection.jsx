044bc4beeb1ac09436463c44dc54784e32a83948
SandeepKumar-05 <sandeepkumarps005@gmail.com>
2025-06-25 19:49:59 +0530

    About Section added

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import '../styles/AboutSection.css'
+function AboutSection() {
+  return (
+    <section className="aboutSection">
+        <div className="aboutContent">
+            <div className="aboutSubCont">
+                <p>I’m a Frontend Developer with a passion for creating smart, user-friendly web interfaces.
+                Skilled in React, JavaScript, and modern UI design.
+                I integrate AI/ML models into real-time web apps using Flask, Node.js, and APIs.
+                From chatbots to intelligent dashboards, I build solutions that think and respond.
+                Focused on crafting seamless, high-performance, AI-powered digital experiences.</p>
+            </div>
+        </div>
+        <div className="aboutSkills">
+            <div className="skillHeading">
+                <h1>How I Can <br /> Assist You</h1>
+            </div>
+            <div className="skillContents">
+                <div className="skill1">
+
+                </div>
+                <div className="skill2">Frontend</div>
+                <div className="skill3">Ai Ml Developer</div>
+                <div className="skill14">Data Analayis</div>
+            </div>
+
+        </div>
+    </section>
+  )
+}
+
+export default AboutSection
