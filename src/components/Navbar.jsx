4ddcf8407890a37a0d91891a39e6ca9a29e11a05
SandeepKumar-05 <sandeepkumarps005@gmail.com>
2025-07-06 20:33:50 +0530

    Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
          <li><Link to="/about">Home</Link></li>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/services">Services</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+       
       </ul>
     </nav>
   );
