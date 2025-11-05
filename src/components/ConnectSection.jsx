5a68c199ff3f121f7d6acff6cb79ff22036cd3a3
SandeepKumar-05 <sandeepkumarps005@gmail.com>
2025-11-05 20:23:02 +0530

    updated

--- src/components/ConnectSection.jsx
+++ src/components/ConnectSection.jsx
@@ -5,13 +5,16 @@ function ConnectSection() {
     <section className='connectSection'>
         <div className='cntContainer'>
             <div className="mainCntCard">
-                <h1>Lets Connect</h1>
+                <h1>Let's Connect and Chat</h1>
             </div>
             <div className="subCntCard">
 
             </div>
         </div>
         <div className='cntContact'>
+          <h1>Sandeep</h1>
+          <p>2025</p>
+          <button>Book a call</button>
 
         </div>
     </section>
--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -4,7 +4,26 @@ export default function ContactSection() {
   return (
     <section className='contSection'>
       <div className="contMainBox">
+        <div className="header">
+          <h1>Contact with me sizzle your project.</h1>
+          <p>Feel free to contant as an techincal friend of yours</p>
+        </div>
 
+          <div className="details">
+            <div className="name">
+              <input type="text" placeholder='Name'/>
+            </div>
+            <div className="email">
+              <input type="email" placeholder='Email'/>
+            </div>
+          </div>
+
+          <div className="desc">
+            <textarea type="text" placeholder='Work Description...' />
+          </div>
+          <div className="subButton">
+            <button type="submit">Submit</button>
+          </div>
       </div>
     </section>
   )
